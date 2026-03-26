e3a34d477d73767fa1780e6ff7bf3553998b79d9
ZeneZ0 <vladk228.sh228@yandex.ru>
2026-03-27 03:11:35 +0800

    update2

--- backend/app/core/init_db.py
+++ backend/app/core/init_db.py
@@ -1,9 +1,12 @@
 from backend.app.core.database import engine, Base
 from backend.app.models.user import User
+from backend.app.models.bot import VKBot
+from backend.app.models.statistic import Statistic
+from backend.app.models.subscription import Subscription
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
-    print("✅ Таблицы созданы")
+    print("✅ Все таблицы созданы")
 
 if __name__ == "__main__":
     create_tables()
